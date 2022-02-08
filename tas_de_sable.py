75049b2d47ee955f29b46de7eb3318ced34acbc2
Gabriel PHILIPPE <gabriel.philippe@ens.uvsq.fr>
2022-02-08 12:36:03 +0100

    InFo

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -7,3 +7,18 @@
 # https://github.com/uvsq22101009/projet_tas_de_sable/blob/main/README.md
 ######################################### 
 
+# import des librairies
+import tkinter as tk
+
+
+# définition des constantes (écrites en majuscule)
+
+
+# définition des variables globales
+
+
+# définition des fonctions (chaque fonction devra contenir une docstring)
+
+
+# programme principal contenant la définition des widgets et des événements qui leur sont liés et l’appel à la boucle de gestion des événements
+
