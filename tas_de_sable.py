aa44aeed9a36347f4497f4b3c729c5ed26ced4de
Wanis Chouaib <wanis.chouaib@ens.uvsq.fr>
2022-02-08 12:34:38 +0100

    .

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,25 +1,9 @@
-# informations liées au groupe
 #########################################
 # groupe n°5 - MIASHS TD2
-# Gabriel PHILIPPE.
-# Aya Saghraoui.
-# Wanis Chouaib.
-# Furkan Toraman.
+# Gabriel PHILIPPE
+# Aya Saghraoui
+# Wanis Chouaib
+# Furkan Toraman
 # https://github.com/uvsq22101009/projet_tas_de_sable/blob/main/README.md
 ######################################### 
-
-# import des librairies
-import tkinter as tk
-
-
-# définition des constantes (écrites en majuscule)
-
-
-# définition des variables globales
-
-
-# définition des fonctions (chaque fonction devra contenir une docstring)
-
-
-# programme principal contenant la définition des widgets et des événements qui leur sont liés et l’appel à la boucle de gestion des événements
 
