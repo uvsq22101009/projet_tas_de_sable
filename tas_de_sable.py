2de3867b7315077083f3cebbf5ebf51b447afc0c
Wanis Chouaib <wanis.chouaib@ens.uvsq.fr>
2022-02-16 23:13:04 +0100

    Modif

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -16,7 +16,7 @@ from random import randint
 
 # définition des variables globales
 grille = [[0,0,0],[0,0,0],[0,0,0]]
-couleur = {1:"grey", 2:"purple", 3:"blue", 4:"green", 5:"yellow", 6:"orange", 7:"red"}
+couleur = {0:"white", 1:"grey", 2:"purple", 3:"blue", 4:"green", 5:"yellow", 6:"orange", 7:"red"}
 
 
 # définition des fonctions (chaque fonction devra contenir une docstring)
@@ -33,7 +33,60 @@ def create_grille():
         for j in range (len(grille)):
             grille[i][j] = randint(0,7)
     print(grille)
-    
+    color_case1()
+    color_case2()
+    color_case3()
+    color_case4()
+    color_case5()
+    color_case6()
+    color_case7()
+    color_case8()
+    color_case9()
+
+
+#Ligne 1
+def color_case1():
+    global grille
+    carre1 = canvas.create_rectangle(0,0,500/3,500/3, width=5, fill=couleur[grille[0][0]])
+    return carre1  
+def color_case2():
+    global grille
+    carre2 = canvas.create_rectangle(500/3,0,500/3*2,500/3, width=5, fill=couleur[grille[0][1]])
+    return carre2
+def color_case3():
+    global grille
+    carre3 = canvas.create_rectangle(500/3*2,0,500,500/3, width=5, fill=couleur[grille[0][2]])
+    return carre3
+
+
+#Ligne 2
+def color_case4():
+    global grille
+    carre4 = canvas.create_rectangle(0,500/3,500/3,500/3*2, width=5, fill=couleur[grille[1][0]])
+    return carre4
+def color_case5():
+    global grille
+    carre5 = canvas.create_rectangle(500/3,500/3,500/3*2,500/3*2, width=5, fill=couleur[grille[1][1]])
+    return carre5
+def color_case6():
+    global grille
+    carre6 = canvas.create_rectangle(500/3*2,500/3,500,500/3*2, width=5, fill=couleur[grille[1][2]])
+    return carre6
+
+
+#Ligne 3
+def color_case7():
+    global grille
+    carre7 = canvas.create_rectangle(0,500/3*2,500/3,500, width=5, fill=couleur[grille[2][0]])
+    return carre7
+def color_case8():
+    global grille
+    carre8 = canvas.create_rectangle(500/3,500/3*2,500/3*2,500, width=5, fill=couleur[grille[2][1]])
+    return carre8
+def color_case9():
+    global grille
+    carre9 = canvas.create_rectangle(500/3*2,500/3*2,500,500, width=5, fill=couleur[grille[2][2]])
+    return carre9
     
 
 def empty_grille():
@@ -131,6 +184,7 @@ racine.title('tas de sable')
 #Explication des codes couleurs,
 #plus il y a de grains plus la couleur est agressive
 print("Association d'une couleur à une valeur d’une configuration :")
+print("0 grain = blanc")
 print("1 grain = gris")
 print("2 grains = violet")
 print("3 grains = bleu")
@@ -159,4 +213,3 @@ canvas.create_line((500/3*2, 0), (500/3*2, 500), fill="gray5", width=5)
 
 
 racine.mainloop() 
-
